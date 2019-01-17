84704e550658001e14c974e294b023932fa057c2
yuryklim <yuryklim@outlook.com>
2019-01-17 23:51:08 +0200

    getting access to the form object created by Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ export class AppComponent {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: ElementRef) {
+  onSubmit(form: NgForm) {
     console.log(form);
   }
 }
