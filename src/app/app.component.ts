1b2d3ce0e77a5da83591ac97313cd77c1c121f89
yuryklim <yuryklim@outlook.com>
2019-01-17 23:37:56 +0200

    using ngSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit() {
+    console.log('Submitted!');
   }
 }
